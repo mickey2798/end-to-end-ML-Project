4ffccff070153b598f2514367bb008464fc8db21
Piyush Kumar Sahoo <32770061+mickey2798@users.noreply.github.com>
2024-03-27 08:49:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import os
-
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
